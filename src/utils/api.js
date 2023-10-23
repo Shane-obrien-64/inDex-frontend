18147269beb7522cf49dbe6c431caeff625900e7
Shane O'Brien <shane.g.obrien@gmail.com>
2023-10-23 15:19:09 -0400

    baseUrl for api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,11 @@
+const baseUrl = "https://pokeapi.co/api/v2/";
+
 const handleServerRes = (res) => {
   return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
 };
 
 const getPokemon = (pokemon) => {
-  return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`).then((res) =>
+  return fetch(`${baseUrl}pokemon/${pokemon}`).then((res) =>
     handleServerRes(res)
   );
 };
